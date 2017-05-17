bf94c14b3d3b48983c8036c1cefbb6cbe64ec976
vbn <zhoucan123@vip.qq.com>
2017-05-17 12:13:19 +0800

    format

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	digital := flag.Int64("d",0,"输入一个10进制数")
-	hexString := flag.String("h","","输入一个16进制数")
+	digital := flag.Int64("d", 0, "输入一个10进制数")
+	hexString := flag.String("h", "", "输入一个16进制数")
 	flag.Parse()
 	if *digital != 0 {
 		var d int64 = *digital
-		hexString := strconv.FormatInt(d,16)
-		fmt.Printf("0x%s\n",hexString)
+		hexString := strconv.FormatInt(d, 16)
+		fmt.Printf("0x%s\n", hexString)
 	} else if *hexString != "" {
-		if strings.HasPrefix(*hexString,"0x") {
-			*hexString = strings.Replace(*hexString,"0x","",1)
+		if strings.HasPrefix(*hexString, "0x") {
+			*hexString = strings.Replace(*hexString, "0x", "", 1)
 		}
-		if digitalString,err := strconv.ParseInt(*hexString,16,0); err != nil {
-			fmt.Printf("error : %@",err)
+		if digitalString, err := strconv.ParseInt(*hexString, 16, 0); err != nil {
+			fmt.Printf("error : %@", err)
 		} else {
-			fmt.Printf("%d\n",digitalString)
+			fmt.Printf("%d\n", digitalString)
 		}
 
 	}

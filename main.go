168e58d4027ee539fdcd316a2b547e770037d2c5
vbn <zhoucan123@vip.qq.com>
2017-05-17 15:49:38 +0800

    support clipboard

--- main.go
+++ main.go
@@ -5,24 +5,34 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"github.com/atotto/clipboard"
 )
 
 func main() {
 	digital := flag.Int64("d", 0, "输入一个10进制数")
 	hexString := flag.String("h", "", "输入一个16进制数")
+	needCopy := flag.Bool("c",false,"将结果复制到剪切板")
 	flag.Parse()
 	if *digital != 0 {
 		var d int64 = *digital
 		hexString := strconv.FormatInt(d, 16)
 		fmt.Printf("0x%s\n", hexString)
+		if *needCopy {
+			hexString = fmt.Sprintf("0x%s", hexString)
+			clipboard.WriteAll(hexString)
+		}
 	} else if *hexString != "" {
 		if strings.HasPrefix(*hexString, "0x") {
 			*hexString = strings.Replace(*hexString, "0x", "", 1)
 		}
-		if digitalString, err := strconv.ParseInt(*hexString, 16, 0); err != nil {
+		if parseNumber, err := strconv.ParseInt(*hexString, 16, 0); err != nil {
 			fmt.Printf("error : %@", err)
 		} else {
-			fmt.Printf("%d\n", digitalString)
+			fmt.Printf("%d\n", parseNumber)
+			if *needCopy {
+				parseStr := fmt.Sprintf("%d", parseNumber)
+				clipboard.WriteAll(parseStr)
+			}
 		}
 
 	}
